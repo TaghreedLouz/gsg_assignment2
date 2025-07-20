f53463d6484714d7f512e01e9a0bafe8dcad7777
TaghreedLouz <taghreedlouz7@gmail.com>
2025-07-21 02:24:40 +0300

    HW2 Q3

--- bin/Q3.dart
+++ bin/Q3.dart
@@ -1,5 +1,19 @@
-main(){
+main() {
+
+  //Declares a constant list [2, 4, 6, 8, 10].
+  const List list = [2, 4, 6, 8, 10];
+  print("List: $list");
+
+  //Converts the list to a set and adds the value 12.
+  Set set = Set.from(list);
+  set.add(12);
+  print("Set: $set");
 
 
+  //Creates a map where keys are from the set and values are the numbers multiplied by 3.
+  Map map = {for (var num in set) num: num * 3};
+
+  //Prints the map.
+  print("Map: $map");
 
 }
